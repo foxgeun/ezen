3081df130f1927046d6da0d21d982242bcc0e60b
foxgeun <p31644@gmail.com>
2023-04-19 09:36:07 +0900

    Summary

--- ezen박수근/javascript_study/과제/js/app.js
+++ ezen박수근/javascript_study/과제/js/app.js
@@ -1,5 +1,5 @@
 // (function () {
-//     //뉴스 api 6a7d9d236bb0436188c4d9581951af69 
+//     //뉴스 api 6a7d9d236bb0436188c4d9581951af69
 //     const text1 = document.querySelector(".text1")
 //     const text2 = document.querySelector(".text2")
 //     const text3 = document.querySelector(".text3")
@@ -37,22 +37,18 @@
 //         console.log("message:" + request.responseText);
 //         console.log("error:" + error);
 //         },
-      
-      
+
 //     });
 
-   
 // }
-
-
 
 // )();
 
 (function () {
   //영화 api b8cfc814abb09ebb64d0f165854e5596
-  
- 
-  const newsAPI = "https://api.themoviedb.org/3/movie/popular?api_key=b8cfc814abb09ebb64d0f165854e5596&language=ko-KR&page=1&region=KR"; // city가 계속 붙으므로 url 초기화를 위해 반드시 넣어준다
+
+  const newsAPI =
+    "https://api.themoviedb.org/3/movie/popular?api_key=b8cfc814abb09ebb64d0f165854e5596&language=ko-KR&page=1&region=KR"; // city가 계속 붙으므로 url 초기화를 위해 반드시 넣어준다
 
   $.ajax({
     type: "GET",
@@ -61,24 +57,24 @@
     async: false, // 결과 데이터를 리턴시키기 위해 동기 방식으로 변경
     success: function (data) {
       console.log(data);
-      
-        for (let i=0; i <= 7; i++){
-            console.log("영화제목: " + data.results[i].title);
-            console.log("영화설명: " + data.results[i].overview);
-            console.log("영화설명: " + data.results[i].id);
-            console.log("영화평점: " + data.results[i].vote_average);
-            const pos = console.log("영화평점: " + data.results[i].poster_path);
 
-            let imgURL =
-              "https://image.tmdb.org/t/p/w500/" + data.results[i].poster_path;
-            $(".imges").append(
-              `<a href="https://www.themoviedb.org/movie/${data.results[i].id}"><img src="${imgURL}"></a>`
-            );
+      for (let i = 0; i <= 7; i++) {
+        console.log("영화제목: " + data.results[i].title);
+        console.log("영화설명: " + data.results[i].overview);
+        console.log("영화설명: " + data.results[i].id);
+        console.log("영화평점: " + data.results[i].vote_average);
+        const pos = console.log("영화평점: " + data.results[i].poster_path);
 
-            $(".imges").append(data.results[i].title);
-        }
-        
-// https://api.themoviedb.org/3/movie/${data.results[i].id}?api_key=b8cfc814abb09ebb64d0f165854e5596&language=ko-KR
+        let imgURL =
+          "https://image.tmdb.org/t/p/w500/" + data.results[i].poster_path;
+        $(".imges").append(
+          `<a href="https://www.themoviedb.org/movie/${data.results[i].id}"><img src="${imgURL}"></a>`
+        );
+
+        $(".imges").append(data.results[i].title);
+      }
+
+      // https://api.themoviedb.org/3/movie/${data.results[i].id}?api_key=b8cfc814abb09ebb64d0f165854e5596&language=ko-KR
     },
     error: function (request, status, error) {
       //응답 에러시 처리 작업
@@ -87,10 +83,4 @@
       console.log("error:" + error);
     },
   });
-}
-
-
-
-)();
-
-
+})();
