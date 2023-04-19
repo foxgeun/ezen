5cc57b952a4175a192419ff3ed919944110ae140
foxgeun <p31644@gmail.com>
2023-04-19 15:19:49 +0900

    Summary

--- ezen박수근/javascript_study/과제/js/app.js
+++ ezen박수근/javascript_study/과제/js/app.js
@@ -47,32 +47,43 @@
 (function () {
   //영화 api b8cfc814abb09ebb64d0f165854e5596
 
-  const newsAPI =
-    "https://api.themoviedb.org/3/movie/popular?api_key=b8cfc814abb09ebb64d0f165854e5596&language=ko-KR&page=1&region=KR"; // city가 계속 붙으므로 url 초기화를 위해 반드시 넣어준다
+  const movieAPI =
+    ("https://api.themoviedb.org/3/movie/popular?api_key=b8cfc814abb09ebb64d0f165854e5596&language=ko-KR&page=1&region=KR"); // city가 계속 붙으므로 url 초기화를 위해 반드시 넣어준다
 
   $.ajax({
     type: "GET",
-    url: newsAPI,
+    url: movieAPI,
     dataType: "json",
     async: false, // 결과 데이터를 리턴시키기 위해 동기 방식으로 변경
     success: function (data) {
       console.log(data);
 
-      for (let i = 0; i <= 7; i++) {
-        console.log("영화제목: " + data.results[i].title);
-        console.log("영화설명: " + data.results[i].overview);
-        console.log("영화설명: " + data.results[i].id);
-        console.log("영화평점: " + data.results[i].vote_average);
-        const pos = console.log("영화평점: " + data.results[i].poster_path);
+      let imgURL = "https://image.tmdb.org/t/p/w500/" + data.results[0].poster_path;
+      console.log(imgURL);
+      $(".movie_img").attr("src", `${imgURL}`)
+      $(".movie_ex").text(data.results[0].title);
 
-        let imgURL =
-          "https://image.tmdb.org/t/p/w500/" + data.results[i].poster_path;
-        $(".imges").append(
-          `<a href="https://www.themoviedb.org/movie/${data.results[i].id}"><img src="${imgURL}"></a>`
-        );
+      // for (let i = 0; i <= 7; i++) {
+      //   console.log("영화제목: " + data.results[i].title);
+      //   console.log("영화설명: " + data.results[i].overview);
+      //   console.log("영화설명: " + data.results[i].id);
+      //   console.log("영화평점: " + data.results[i].vote_average);
+      //   const pos = console.log("영화평점: " + data.results[i].poster_path);
 
-        $(".imges").append(data.results[i].title);
-      }
+      //   let imgURL =
+      //     "https://image.tmdb.org/t/p/w500/" + data.results[i].poster_path;
+      //   $(".imges").append(
+      //     `<a href="https://www.themoviedb.org/movie/${data.results[i].id}"><img src="${imgURL}"></a>`
+      //   );
+
+      //   $(".imges").append(data.results[i].title);
+
+      //   if (data.results[i].genre_ids[i] == 16) {
+      //     console.log(data.results[i]);
+      //   }
+
+      //   ketword = `https://api.themoviedb.org/3/movie/${data.results[i].id}/b8cfc814abb09ebb64d0f165854e5596`;
+      // }
 
       // https://api.themoviedb.org/3/movie/${data.results[i].id}?api_key=b8cfc814abb09ebb64d0f165854e5596&language=ko-KR
     },
@@ -83,4 +94,64 @@
       console.log("error:" + error);
     },
   });
+  const movie2API =
+    "https://api.themoviedb.org/3/movie/popular?api_key=b8cfc814abb09ebb64d0f165854e5596&language=ko-KR&page=2&region=KR"; // city가 계속 붙으므로 url 초기화를 위해 반드시 넣어준다
+
+  $.ajax({
+    type: "GET",
+    url: movie2API,
+    dataType: "json",
+    async: false, // 결과 데이터를 리턴시키기 위해 동기 방식으로 변경
+    success: function (data) {
+      console.log(data);
+
+     
+    },
+    error: function (request, status, error) {
+      //응답 에러시 처리 작업
+      console.log("code:" + request.status);
+      console.log("message:" + request.responseText);
+      console.log("error:" + error);
+    },
+  });
+
+   const movie_now_API =
+     "https://api.themoviedb.org/3/movie/now_playing?api_key=b8cfc814abb09ebb64d0f165854e5596&language=ko-KR&page=1&region=KR"; // city가 계속 붙으므로 url 초기화를 위해 반드시 넣어준다
+
+   $.ajax({
+     type: "GET",
+     url: movie_now_API,
+     dataType: "json",
+     async: false, // 결과 데이터를 리턴시키기 위해 동기 방식으로 변경
+     success: function (data) {
+       console.log(data);
+      const i =0
+
+       
+         let imgURL =
+           "https://image.tmdb.org/t/p/w500/" + data.results[i].poster_path;
+         console.log(imgURL);
+         $(".now_img").attr("src", `${imgURL}`);
+         $(".now_ex").text(data.results[0].title);
+        
+
+       
+
+
+
+
+
+
+
+
+     },
+     error: function (request, status, error) {
+       //응답 에러시 처리 작업
+       console.log("code:" + request.status);
+       console.log("message:" + request.responseText);
+       console.log("error:" + error);
+     },
+   });
+
+
 })();
