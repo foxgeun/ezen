8cd75113ec462785c7e00a53d0acf837231398b1
foxgeun <p31644@gmail.com>
2023-04-21 18:11:48 +0900

    Summary

--- ezen박수근/javascript_study/과제/js/app.js
+++ ezen박수근/javascript_study/과제/js/app.js
@@ -1,11 +1,6 @@
 (function () {
   //영화 api b8cfc814abb09ebb64d0f165854e5596
 
- 
-  
-  
-  
- 
   var movieAPI = `https://api.themoviedb.org/3/movie/popular?api_key=b8cfc814abb09ebb64d0f165854e5596&language=ko-KR&page=1&region=KR`; // city가 계속 붙으므로 url 초기화를 위해 반드시 넣어준다
 
   $.ajax({
@@ -19,25 +14,25 @@
       for (let i = 0; i <= 20; i++) {
         let imgURL =
           "https://image.tmdb.org/t/p/w500/" + data.results[i].poster_path;
-           
+
         $(".movie_link2").append(
-             `<a href="https://www.themoviedb.org/movie/${data.results[i].id}"><img class="movie_img" src="${imgURL}"></img><h1 class="movie_ex">${data.results[i].title}</h1></a>`
+          `<a class="moive_atag" href="https://www.themoviedb.org/movie/${data.results[i].id}"><img class="movie_img" src="${imgURL}"></img><h1 class="movie_ex">${data.results[i].title}</h1></a>`
         );
-        
-        let bn1 = document.querySelector(".bn1")
-        bn1.addEventListener("click",ss)
+
+        let bn1 = document.querySelector(".bn1");
+        bn1.addEventListener("click", ss);
 
         function ss() {
           $(".mi").remove();
           $(".ho").remove();
           $(".all").remove();
-         for (let e = 0; e <= 5; e++) {
-           if (data.results[i].genre_ids[e] == 16) {
-             $(".movie_con").append(
-               `<a class=act href="https://www.themoviedb.org/movie/${data.results[i].id}"><img class="movie_img" src="${imgURL}"></img><h1 class="movie_ex">${data.results[i].title}</h1></a>`
-             );
-           }
-         }
+          for (let e = 0; e <= 5; e++) {
+            if (data.results[i].genre_ids[e] == 16) {
+              $(".movie_con").append(
+                `<a class="act moive_atag" href="https://www.themoviedb.org/movie/${data.results[i].id}"><img class="movie_img" src="${imgURL}"></img><h1 class="movie_ex">${data.results[i].title}</h1></a>`
+              );
+            }
+          }
         }
         let bn2 = document.querySelector(".bn2");
         bn2.addEventListener("click", dd);
@@ -48,7 +43,7 @@
           for (let e = 0; e <= 5; e++) {
             if (data.results[i].genre_ids[e] == 28) {
               $(".movie_con1").append(
-                `<a class=mi href="https://www.themoviedb.org/movie/${data.results[i].id}"><img class="movie_img" src="${imgURL}"></img><h1 class="movie_ex">${data.results[i].title}</h1></a>`
+                `<a class="mi moive_atag" href="https://www.themoviedb.org/movie/${data.results[i].id}"><img class="movie_img" src="${imgURL}"></img><h1 class="movie_ex">${data.results[i].title}</h1></a>`
               );
             }
           }
@@ -58,18 +53,18 @@
         function qq() {
           $(".act").remove();
           $(".mi").remove();
-           $(".all").remove();
+          $(".all").remove();
           for (let e = 0; e <= 5; e++) {
             if (data.results[i].genre_ids[e] == 27) {
               $(".movie_con2").append(
-                `<a class=ho href="https://www.themoviedb.org/movie/${data.results[i].id}"><img class="movie_img" src="${imgURL}"></img><h1 class="movie_ex">${data.results[i].title}</h1></a>`
+                `<a class="ho moive_atag" href="https://www.themoviedb.org/movie/${data.results[i].id}"><img class="movie_img" src="${imgURL}"></img><h1 class="movie_ex">${data.results[i].title}</h1></a>`
               );
             }
           }
         }
-        
-          $(".movie_con3").append(
-            `<a class=all href="https://www.themoviedb.org/movie/${data.results[i].id}"><img class="movie_img" src="${imgURL}"></img><h1 class="movie_ex">${data.results[i].title}</h1></a>`
+
+        $(".movie_con3").append(
+          `<a class=all href="https://www.themoviedb.org/movie/${data.results[i].id}"><img class="movie_img" src="${imgURL}"></img><h1 class="movie_ex">${data.results[i].title}</h1></a>`
         );
         let bn4 = document.querySelector(".bn4");
         bn4.addEventListener("click", mm);
@@ -77,15 +72,11 @@
           $(".act").remove();
           $(".mi").remove();
           $(".ho").remove();
-          
-              $(".movie_con3").append(
-                `<a class=all href="https://www.themoviedb.org/movie/${data.results[i].id}"><img class="movie_img" src="${imgURL}"></img><h1 class="movie_ex">${data.results[i].title}</h1></a>`
-              );
-            
-          }
-        
-        
-        
+
+          $(".movie_con3").append(
+            `<a class="all moive_atag" href="https://www.themoviedb.org/movie/${data.results[i].id}"><img class="movie_img" src="${imgURL}"></img><h1 class="movie_ex">${data.results[i].title}</h1></a>`
+          );
+        }
       }
     },
     error: function (request, status, error) {
@@ -106,13 +97,12 @@
     success: function (data) {
       console.log(data);
 
-      
       for (let i = 0; i <= 20; i++) {
         let imgURL =
           "https://image.tmdb.org/t/p/w500/" + data.results[i].poster_path;
 
         $(".movie_link2").append(
-          `<a href="https://www.themoviedb.org/movie/${data.results[i].id}"><img class="movie_img" src="${imgURL}"></img><h1 class="movie_ex">${data.results[i].title}</h1></a>`
+          `<a class="moive_atag" href="https://www.themoviedb.org/movie/${data.results[i].id}"><img class="movie_img" src="${imgURL}"></img><h1 class="movie_ex">${data.results[i].title}</h1></a>`
         );
 
         let bn1 = document.querySelector(".bn1");
@@ -125,7 +115,7 @@
           for (let e = 0; e <= 5; e++) {
             if (data.results[i].genre_ids[e] == 16) {
               $(".movie_con").append(
-                `<a class=act href="https://www.themoviedb.org/movie/${data.results[i].id}"><img class="movie_img" src="${imgURL}"></img><h1 class="movie_ex">${data.results[i].title}</h1></a>`
+                `<a class="act moive_atag" href="https://www.themoviedb.org/movie/${data.results[i].id}"><img class="movie_img" src="${imgURL}"></img><h1 class="movie_ex">${data.results[i].title}</h1></a>`
               );
             }
           }
@@ -139,7 +129,7 @@
           for (let e = 0; e <= 5; e++) {
             if (data.results[i].genre_ids[e] == 28) {
               $(".movie_con1").append(
-                `<a class=mi href="https://www.themoviedb.org/movie/${data.results[i].id}"><img class="movie_img" src="${imgURL}"></img><h1 class="movie_ex">${data.results[i].title}</h1></a>`
+                `<a class="mi moive_atag" href="https://www.themoviedb.org/movie/${data.results[i].id}"><img class="movie_img" src="${imgURL}"></img><h1 class="movie_ex">${data.results[i].title}</h1></a>`
               );
             }
           }
@@ -153,13 +143,13 @@
           for (let e = 0; e <= 5; e++) {
             if (data.results[i].genre_ids[e] == 27) {
               $(".movie_con2").append(
-                `<a class=ho href="https://www.themoviedb.org/movie/${data.results[i].id}"><img class="movie_img" src="${imgURL}"></img><h1 class="movie_ex">${data.results[i].title}</h1></a>`
+                `<a class="ho moive_atag" href="https://www.themoviedb.org/movie/${data.results[i].id}"><img class="movie_img" src="${imgURL}"></img><h1 class="movie_ex">${data.results[i].title}</h1></a>`
               );
             }
           }
         }
-      $(".movie_con3").append(
-        `<a class=all href="https://www.themoviedb.org/movie/${data.results[i].id}"><img class="movie_img" src="${imgURL}"></img><h1 class="movie_ex">${data.results[i].title}</h1></a>`
+        $(".movie_con3").append(
+          `<a class="all" href="https://www.themoviedb.org/movie/${data.results[i].id}"><img class="movie_img" src="${imgURL}"></img><h1 class="movie_ex">${data.results[i].title}</h1></a>`
         );
         let bn4 = document.querySelector(".bn4");
         bn4.addEventListener("click", mm);
@@ -169,7 +159,7 @@
           $(".ho").remove();
 
           $(".movie_con3").append(
-            `<a class=all href="https://www.themoviedb.org/movie/${data.results[i].id}"><img class="movie_img" src="${imgURL}"></img><h1 class="movie_ex">${data.results[i].title}</h1></a>`
+            `<a class="all moive_atag" href="https://www.themoviedb.org/movie/${data.results[i].id}"><img class="movie_img" src="${imgURL}"></img><h1 class="movie_ex">${data.results[i].title}</h1></a>`
           );
         }
       }
@@ -192,72 +182,72 @@
     success: function (data) {
       console.log(data);
 
-       for (let i = 0; i <= 20; i++) {
-         let imgURL =
-           "https://image.tmdb.org/t/p/w500/" + data.results[i].poster_path;
+      for (let i = 0; i <= 20; i++) {
+        let imgURL =
+          "https://image.tmdb.org/t/p/w500/" + data.results[i].poster_path;
 
-         $(".movie_link2").append(
-           `<a href="https://www.themoviedb.org/movie/${data.results[i].id}"><img class="movie_img" src="${imgURL}"></img><h1 class="movie_ex">${data.results[i].title}</h1></a>`
-         );
+        $(".movie_link2").append(
+          `<a class="moive_atag" href="https://www.themoviedb.org/movie/${data.results[i].id}"><img class="movie_img" src="${imgURL}"></img><h1 class="movie_ex">${data.results[i].title}</h1></a>`
+        );
 
-         let bn1 = document.querySelector(".bn1");
-         bn1.addEventListener("click", ss);
+        let bn1 = document.querySelector(".bn1");
+        bn1.addEventListener("click", ss);
 
-         function ss() {
-           $(".mi").remove();
-           $(".ho").remove();
-           $(".all").remove();
-           for (let e = 0; e <= 5; e++) {
-             if (data.results[i].genre_ids[e] == 16) {
-               $(".movie_con").append(
-                 `<a class=act href="https://www.themoviedb.org/movie/${data.results[i].id}"><img class="movie_img" src="${imgURL}"></img><h1 class="movie_ex">${data.results[i].title}</h1></a>`
-               );
-             }
-           }
-         }
-         let bn2 = document.querySelector(".bn2");
-         bn2.addEventListener("click", dd);
-         function dd() {
-           $(".act").remove();
-           $(".ho").remove();
-           $(".all").remove();
-           for (let e = 0; e <= 5; e++) {
-             if (data.results[i].genre_ids[e] == 28) {
-               $(".movie_con1").append(
-                 `<a class=mi href="https://www.themoviedb.org/movie/${data.results[i].id}"><img class="movie_img" src="${imgURL}"></img><h1 class="movie_ex">${data.results[i].title}</h1></a>`
-               );
-             }
-           }
-         }
-         let bn3 = document.querySelector(".bn3");
-         bn3.addEventListener("click", qq);
-         function qq() {
-           $(".act").remove();
-           $(".mi").remove();
+        function ss() {
+          $(".mi").remove();
+          $(".ho").remove();
           $(".all").remove();
-           for (let e = 0; e <= 5; e++) {
-             if (data.results[i].genre_ids[e] == 27) {
-               $(".movie_con2").append(
-                 `<a class=ho href="https://www.themoviedb.org/movie/${data.results[i].id}"><img class="movie_img" src="${imgURL}"></img><h1 class="movie_ex">${data.results[i].title}</h1></a>`
-               );
-             }
-           }
-         }
-      $(".movie_con3").append(
-        `<a class=all href="https://www.themoviedb.org/movie/${data.results[i].id}"><img class="movie_img" src="${imgURL}"></img><h1 class="movie_ex">${data.results[i].title}</h1></a>`
-         );
-         let bn4 = document.querySelector(".bn4");
-         bn4.addEventListener("click", mm);
-         function mm() {
-           $(".act").remove();
-           $(".mi").remove();
-           $(".ho").remove();
+          for (let e = 0; e <= 5; e++) {
+            if (data.results[i].genre_ids[e] == 16) {
+              $(".movie_con").append(
+                `<a class="act moive_atag" href="https://www.themoviedb.org/movie/${data.results[i].id}"><img class="movie_img" src="${imgURL}"></img><h1 class="movie_ex">${data.results[i].title}</h1></a>`
+              );
+            }
+          }
+        }
+        let bn2 = document.querySelector(".bn2");
+        bn2.addEventListener("click", dd);
+        function dd() {
+          $(".act").remove();
+          $(".ho").remove();
+          $(".all").remove();
+          for (let e = 0; e <= 5; e++) {
+            if (data.results[i].genre_ids[e] == 28) {
+              $(".movie_con1").append(
+                `<a class="mi moive_atag" href="https://www.themoviedb.org/movie/${data.results[i].id}"><img class="movie_img" src="${imgURL}"></img><h1 class="movie_ex">${data.results[i].title}</h1></a>`
+              );
+            }
+          }
+        }
+        let bn3 = document.querySelector(".bn3");
+        bn3.addEventListener("click", qq);
+        function qq() {
+          $(".act").remove();
+          $(".mi").remove();
+          $(".all").remove();
+          for (let e = 0; e <= 5; e++) {
+            if (data.results[i].genre_ids[e] == 27) {
+              $(".movie_con2").append(
+                `<a class="ho moive_atag" href="https://www.themoviedb.org/movie/${data.results[i].id}"><img class="movie_img" src="${imgURL}"></img><h1 class="movie_ex">${data.results[i].title}</h1></a>`
+              );
+            }
+          }
+        }
+        $(".movie_con3").append(
+          `<a class=all href="https://www.themoviedb.org/movie/${data.results[i].id}"><img class="movie_img" src="${imgURL}"></img><h1 class="movie_ex">${data.results[i].title}</h1></a>`
+        );
+        let bn4 = document.querySelector(".bn4");
+        bn4.addEventListener("click", mm);
+        function mm() {
+          $(".act").remove();
+          $(".mi").remove();
+          $(".ho").remove();
 
-           $(".movie_con3").append(
-             `<a class=all href="https://www.themoviedb.org/movie/${data.results[i].id}"><img class="movie_img" src="${imgURL}"></img><h1 class="movie_ex">${data.results[i].title}</h1></a>`
-           );
-         }
-       }
+          $(".movie_con3").append(
+            `<a class="all moive_atag" href="https://www.themoviedb.org/movie/${data.results[i].id}"><img class="movie_img" src="${imgURL}"></img><h1 class="movie_ex">${data.results[i].title}</h1></a>`
+          );
+        }
+      }
     },
     error: function (request, status, error) {
       //응답 에러시 처리 작업
